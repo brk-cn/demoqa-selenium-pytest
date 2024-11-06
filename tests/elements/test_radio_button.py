c1744e877400364dd523ce21c33dec803c75d08a
Burak Can <burakcanswe@gmail.com>
2024-11-06 14:13:59 +0300

    fix test radio button

--- tests/elements/test_radio_button.py
+++ tests/elements/test_radio_button.py
@@ -1,3 +1,4 @@
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,7 +11,7 @@ class TestRadioButton:
         self.driver.get("https://demoqa.com/radio-button")
 
         yes_radio_label = self.driver.find_element(By.XPATH, "//label[@for='yesRadio']")
-        yes_radio_label.click()
+        self.driver.execute_script("arguments[0].click();", yes_radio_label)
 
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "text-success")))
         spans = self.driver.find_elements(By.CLASS_NAME, "text-success")
@@ -22,7 +23,7 @@ class TestRadioButton:
         self.driver.get("https://demoqa.com/radio-button")
 
         impressive_radio_label = self.driver.find_element(By.XPATH, "//label[@for='impressiveRadio']")
-        impressive_radio_label.click()
+        self.driver.execute_script("arguments[0].click();", impressive_radio_label)
 
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "text-success")))
         spans = self.driver.find_elements(By.CLASS_NAME, "text-success")
